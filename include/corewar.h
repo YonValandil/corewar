abdaa3d90ed37e6a43a0923297290dc3115c28a7
Jimmy JOURNE <jjourne@e2r11p13.42.fr>
2018-06-04 03:00:40 +0200

    start project

---
+++ include/corewar.h
@@ -0,0 +1,19 @@
+#ifndef COREWAR_H
+# define COREWAR_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdint.h>
+# include <stdarg.h>
+# include <limits.h>
+# include "../libft/include/libft.h"
+# include "../libft/include/get_next_line.h"
+# include "../libft/include/ft_printf.h"
+
+# define BUF_SIZE 4096
+
+//typedef struct		s_corewar
+//{
+//}					t_corewar;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include "corewar.h"
+
+void	exit_error(const char *s)
+{
+	ft_putendl_fd(s, 2);
+	exit(EXIT_FAILURE);
+}
+
+int		main(int argc, char *argv[])
+{
+	t_lemin		lemin;
+	t_room		*room;
+
+	(void)argv;
+	if (argc != 1)
+		exit_error("ERROR");
+	return (0);
+}
